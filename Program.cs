c66ced096cb92baebdb1893c71e400b276649bc9
d7ry <92609548+D7ry@users.noreply.github.com>
2021-12-10 00:31:30 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAR_Equal_Chance_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("How many DAR folders?");
+                float folderCt = float.Parse(Console.ReadLine());
+                float target_P = 1 / folderCt;
+                float overwriteP = 1.0f;
+                while (folderCt >= 1)
+                {
+                    float chance = target_P / overwriteP;
+                    Console.WriteLine(chance);
+                    folderCt--;
+                    overwriteP *= (1 - chance);
+                }
+            }
+        }
+    }
+}
